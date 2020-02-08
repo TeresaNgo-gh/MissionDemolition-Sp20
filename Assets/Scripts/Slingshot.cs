b1c258b5f0a48c55c714fcc8eb605e8abbbaa605
TeresaNgo-gh <57918864+TeresaNgo-gh@users.noreply.github.com>
2020-02-08 11:55:59 -0500

    camera follows projectile

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -62,6 +62,7 @@ public class Slingshot : MonoBehaviour{
             aimingMode = false;
             projectileRigidbody.isKinematic = false;
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
             projectile = null;
         }
     }
