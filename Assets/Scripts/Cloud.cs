412b18e98073a8ce9607b64a47597785184a6966
TeresaNgo-gh <57918864+TeresaNgo-gh@users.noreply.github.com>
2020-02-08 12:17:36 -0500

    made a cloud prefab

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject cloudSphere;
+    public int numSpheresMin = 6;
+    public int numSpheresMax = 10;
+    public Vector3 sphereOffsetScale = new Vector3(5,2,1);
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
